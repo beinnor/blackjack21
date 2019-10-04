f03d87be43c63f466a5abf2ecb366b5aa10c85c8
Ronnie <16521174+beinnor@users.noreply.github.com>
2019-10-04 12:06:14 +0200

    Updates

--- static/js/main.e6453301.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpblackjack21=this.webpackJsonpblackjack21||[]).push([[0],{3:function(e,n,t){e.exports=t(9)},8:function(e,n,t){},9:function(e,n,t){"use strict";t.r(n);var a=t(0),c=t.n(a),l=t(2),r=t.n(l);t(8);var o=function(){return c.a.createElement("div",{className:"App"},c.a.createElement("h1",null,"hello"))};r.a.render(c.a.createElement(o,null),document.getElementById("root"))}},[[3,1,2]]]);
-//# sourceMappingURL=main.e6453301.chunk.js.map
